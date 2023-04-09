6a1196b282593c57ebcbd2763afcb2a23b1692f4
celikkalper <88833171+celikkalper@users.noreply.github.com>
2023-04-09 04:14:14 +0300

    2

--- Program.cs
+++ Program.cs
@@ -66,8 +66,8 @@
             Console.WriteLine(sonuc1);
             sonuc1 = sayi1 + sayi2;
             Console.WriteLine(sonuc1);
-            sonuc1 = sayi1++;
-            Console.WriteLine(sayi1);
+            sonuc1 = ++sayi1;
+            Console.WriteLine(sonuc1);
             int sonuc2 = 20 % 3;
             Console.WriteLine(sonuc2);
 
